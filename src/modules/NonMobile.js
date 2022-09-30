4f3d01e6bc7be3ffa8678ba1399da24e7c4fdf6f
evkim1019 <kimgem89@gmail.com>
2022-09-30 01:55:34 -0400

    added logo on nonMobile screen

--- src/modules/NonMobile.js
+++ src/modules/NonMobile.js
@@ -8,11 +8,13 @@ function NonMobile() {
   return (
     <div className="nonMobile">
       <div className="nonMobile__content">
+
+        <p className="nonMobile_logo">Advanced Analgesia</p>
+        <br />
         <h1>Scan QR code below</h1>
         <p>For the full experience, please scan the QR code with your mobile phone.</p>
         <br /><br />
         <img className="nonMobile__content-qr" src={`${qrCode}`} alt="" />
-
       </div>
     </div>
   )
